fd1a95e40b81ad051468c4b49ab5dd2b97b7afc1
Elina Jaimes <ev.jaimes@uniandes.edu.co>
2021-04-11 19:41:29 -0500

    Agregadas cositas

--- app-back/front/src/components/Job.js
+++ app-back/front/src/components/Job.js
@@ -1,6 +1,25 @@
 import React from "react";
 
-const Job = () => {
-  return <div>This is a job offer.</div>;
+const state = {
+  name: "Asesor comercial de hipermercado",
+  company: "Schneider Electric",
+  salary: "$4.5 a $5.5 millones",
+  city: "Bogotá, Colombia",
 };
+
+const Job = () => {
+  return <div>{renderOffer()}</div>;
+};
+
+const renderOffer = () => {
+  return (
+    <div>
+      <h2>{state.name}</h2>
+      <h3>{state.company}</h3>
+      <h4>{state.salary}</h4>
+      <h5>{state.city}</h5>
+    </div>
+  );
+};
+
 export default Job;
